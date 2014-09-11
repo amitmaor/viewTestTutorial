63664071f0b12313d95d873c3d8ce3131aa4cbac
Amit Maor <amaor16@cmc.edu>
2014-09-11 10:14:42 -0700

    Initial Commit

---
+++ ViewTest/AMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AMAppDelegate.h
+//  ViewTest
+//
+//  Created by Amit Maor on 9/11/14.
+//  Copyright (c) 2014 Amit Maor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewTest
+//
+//  Created by Amit Maor on 9/11/14.
+//  Copyright (c) 2014 Amit Maor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AMAppDelegate class]));
+    }
+}
